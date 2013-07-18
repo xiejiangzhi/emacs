611ebc15a370ea0b371c6fe354791de3a191d202
jiangzhi.xie <jiangzhi.xie@edoctor.cn>
2013-07-18 18:34:13 +0800

    undo-tree

--- init.el
+++ init.el
@@ -5,3 +5,13 @@
 (require 'linum)
 (global-linum-mode 1)
 
+
+(set-language-environment 'UTF-8)
+(set-locale-environment "UTF-8")
+
+(setq default-frame-alist 
+  '((height . 40) (width . 128) (tool-bar-lines . 0)
+)) 
+
+(require 'undo-tree)
+(global-undo-tree-mode)
