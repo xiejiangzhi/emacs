db0d0b5519cbde697a43f874efd0c6a04cc61c33
jiangzhi.xie <jiangzhi.xie@edoctor.cn>
2013-12-03 17:05:25 +0800

    Add markdown and whitespace
    markdown-mode
    whitespace-mode
    add Gemfile to ruby-mode
    add *.jbuilder to ruby-mode

--- init.el
+++ init.el
@@ -1,12 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/lib/")
 
-(require `dirtree)
-(global-set-key "\C-o" 'dirtree-show)
 
-(require 'linum)
-(global-linum-mode 1)
-
-
+;; Emacs env
 (set-language-environment 'UTF-8)
 (set-locale-environment "UTF-8")
 
@@ -14,10 +9,43 @@
   '((height . 40) (width . 128) (tool-bar-lines . 0)
 )) 
 
+(set-frame-parameter (selected-frame) 'alpha (list 95 90))
+(add-to-list 'default-frame-alist (cons 'alpha (list 95 90)))
+
+(setq ruby-deep-indent-paren-style nil)
+
+
+
+
+;; Plugins
+(require `dirtree)
+(global-set-key "\C-o" 'dirtree-show)
+
+(require 'linum)
+(global-linum-mode 1)
+
 (require 'undo-tree)
 (global-undo-tree-mode)
 
-(set-frame-parameter (selected-frame) 'alpha (list 90 50))
-(add-to-list 'default-frame-alist (cons 'alpha (list 90 50)))
+(require 'whitespace)
+(setq whitespace-style 
+  (quote (face spaces tabs space-mark tab-mark))
+)
+(global-whitespace-mode 1)
 
-(setq ruby-deep-indent-paren-style nil)
+
+
+;; Edit mode
+
+; markdown
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+
+; jbuilder
+(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+
+
